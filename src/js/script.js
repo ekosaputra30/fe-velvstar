86e4206e770fc1b0894f347701a966209726d959
eko saputra <e.poetra92@gmail.com>
2022-09-30 07:28:34 +0800

    feat(header): shrink navbar on scroll function

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 import "bootstrap";
+
+// const onShrinkLimit = window.innerHeight / 2;
+const onShrinkLimit = 100;
+
+window.onscroll = () => {
+  if (
+    document.body.scrollTop > onShrinkLimit ||
+    document.documentElement.scrollTop > onShrinkLimit
+  ) {
+    document.querySelector("header").classList.add("is-shrink");
+  } else {
+    document.querySelector("header").classList.remove("is-shrink");
+  }
+};
